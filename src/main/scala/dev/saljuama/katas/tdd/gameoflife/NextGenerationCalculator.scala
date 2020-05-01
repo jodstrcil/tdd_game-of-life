72972a0d6708b727fe9be69894d68d93fdc1067c
Salvador Juan <sjuan@thoughtworks.com>
2020-05-01 17:30:10 +0200

    add underpopulation and overcrowding rules

--- src/main/scala/dev/saljuama/katas/tdd/gameoflife/NextGenerationCalculator.scala
+++ src/main/scala/dev/saljuama/katas/tdd/gameoflife/NextGenerationCalculator.scala
@@ -3,7 +3,23 @@ package dev.saljuama.katas.tdd.gameoflife
 class NextGenerationCalculator {
 
   def nextGeneration(generation: Generation): Generation = {
-    Generation(generation.number+1, generation.rows, generation.columns, generation.life)
+    val nextLife =
+      for (row <- 0 until generation.rows)
+        yield for (col <- 0 until generation.columns)
+          yield cellStateInNextGeneration(row, col, generation)
+    Generation(generation.number + 1, generation.rows, generation.columns, nextLife)
   }
 
+  private def cellStateInNextGeneration(row: Int, col: Int, currentGeneration: Generation) = {
+    val aliveNeighbours = (for (
+      r <- row - 1 to row + 1 if r >= 0 && r < currentGeneration.rows;
+      c <- col - 1 to col + 1 if c >= 0 && c < currentGeneration.columns
+    ) yield (r, c))
+      .filterNot(_ == (row, col))
+      .map { n => currentGeneration.life(n._1)(n._2) }
+      .count(_ == true)
+
+    if (aliveNeighbours == 2 || aliveNeighbours == 3) currentGeneration.life(row)(col)
+    else false
+  }
 }
--- src/test/scala/dev/saljuama/katas/tdd/gameoflife/NextGenerationCalculatorUnitTest.scala
+++ src/test/scala/dev/saljuama/katas/tdd/gameoflife/NextGenerationCalculatorUnitTest.scala
@@ -17,6 +17,80 @@ class NextGenerationCalculatorUnitTest extends AnyWordSpec with Matchers {
         next.number must be(2)
       }
 
+      "find cells that will die because of underpopulation" when {
+
+        "cells do not have any alive neighbours" in {
+          val current = generation(1, Seq(
+            "*.",
+            ".."
+          ))
+          val expectedNext = generation(2, Seq(
+            "..",
+            ".."
+          ))
+          calculator.nextGeneration(current) must equal(expectedNext)
+        }
+
+        "cells only have one alive neighbour" in {
+          val current = generation(1, Seq(
+            "...",
+            ".**",
+            "..."
+          ))
+          val expectedNext = generation(2, Seq(
+            "...",
+            "...",
+            "..."
+          ))
+          calculator.nextGeneration(current) must equal(expectedNext)
+        }
+
+      }
+
+      "find cells that will stay alive" when {
+
+        "cells have two alive neighbours" in {
+          val current = generation(1, Seq(
+            ".*",
+            "**"
+          ))
+          val expectedNext = generation(2, Seq(
+            ".*",
+            "**"
+          ))
+          calculator.nextGeneration(current) must equal(expectedNext)
+        }
+
+        "cells have three alive neighbours" in {
+          val current = generation(1, Seq(
+            "**",
+            "**"
+          ))
+          val expectedNext = generation(2, Seq(
+            "**",
+            "**"
+          ))
+          calculator.nextGeneration(current) must equal(expectedNext)
+        }
+      }
+
+      "find cells that will die because of overcrowding" when {
+
+        "cells have more than three alive neighbours" in {
+          val current = generation(1, Seq(
+            ".*.",
+            "***",
+            ".*."
+          ))
+          val expectedNext = generation(2, Seq(
+            ".*.",
+            "*.*",
+            ".*."
+          ))
+          calculator.nextGeneration(current) must equal(expectedNext)
+        }
+      }
+
     }
   }
 }
